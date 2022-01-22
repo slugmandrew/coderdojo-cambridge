5b979a8776a1895289c3e76ba0302d14814f2777
Drew Spencer <slugmandrew@gmail.com>
2022-01-22 13:23:31 +0000

    Initial basic framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Flex, Heading, VStack, Text, Link } from "@chakra-ui/react";
+import data from "./Data"
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxW={"container.xl"} bgColor={"gray.50"}>
+      <Flex padding={10}>
+        <VStack w={"full"} h={"full"}>
+          <Heading size={"2xl"}>CoderDojo Cambridge</Heading>
+          <Flex>
+            <Heading size={"lg"} paddingY={3}>Projects List</Heading>
+          </Flex>
+
+          {data.map(proj => (
+            <>
+              <Heading size={"md"} paddingY={3}>{proj.title}</Heading>
+              <Text>{proj.language}</Text>
+              <Link href={proj.url} target={"_blank"}>{proj.url}</Link>
+            </>
+
+          ))}
+
+        </VStack>
+      </Flex>
+    </Container>
   );
 }
 
---
+++ src/Data.ts
@@ -0,0 +1,25 @@
+type Language = "Scratch" | "Python" | "Unity" | "Java" | "HTML";
+
+type Level = "Beginner" | "Intermediate" | "Advanced"
+
+type Project = {
+  url: string
+  title: string
+  level: Level
+  language: Language
+}
+
+const getProjects: () => Project[] = () => {
+  const projects: Project[] = [
+    { language: "Scratch", level: "Beginner", title: "Space Talk", url: "https://projects.raspberrypi.org/en/projects/space-talk" },
+    { language: "Scratch", level: "Intermediate", title: "Broadcasting spells", url: "https://projects.raspberrypi.org/en/projects/broadcasting-spells" },
+    { language: "Scratch", level: "Beginner", title: "", url: "" },
+    { language: "Java", level: "Beginner", title: "", url: "" },
+    { language: "Unity", level: "Beginner", title: "", url: "" },
+    { language: "HTML", level: "Beginner", title: "", url: "" },
+  ]
+
+  return projects
+}
+
+export default getProjects()
