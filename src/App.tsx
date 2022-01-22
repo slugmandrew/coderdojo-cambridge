e768c6e374344e06c220ec5c018dd1ee3133035a
Drew Spencer <slugmandrew@gmail.com>
2022-01-22 16:19:05 +0000

    Tidy headings

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,51 @@
 import React from 'react'
 import {
+  Box,
   Container,
   Flex,
   Heading,
-  VStack,
-  Text,
-  Link,
-  Box,
+  HStack,
   Image,
+  Link,
+  Text,
+  VStack,
 } from '@chakra-ui/react'
 import data from './Data'
 import logo from './coderdojo-cambridge-logo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMousePointer } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
     <Container maxW={'container.xl'} bgColor={'gray.50'} h={'100vh'}>
       <Flex w={'full'} h={'full'} padding={10}>
         <VStack spacing={10} ali w={'full'} h={'full'}>
-          <Image src={logo} w={200} />
+          <HStack spacing={5}>
+            <Image src={logo} w={100} />
+            <VStack>
+              <Heading size={'2xl'}>CoderDojo Cambridge</Heading>
+              <Heading alignSelf={'flex-start'} size={'md'}>
+                Projects, resources & other cool stuff
+              </Heading>
+            </VStack>
+          </HStack>
 
-          <Heading size={'2xl'}>CoderDojo Cambridge</Heading>
-          <Heading size={'lg'} paddingY={3}>
-            <Link
-              href={'https://coderdojo-cambridge.herokuapp.com/'}
-              target={'_blank'}
-            >
-              coderdojo-cambridge.herokuapp.com
-            </Link>
-          </Heading>
+          <HStack spacing={5}>
+            <Heading size={'lg'} paddingY={3}>
+              🖱{' '}
+              <Link
+                size={'lg'}
+                color={'#E07BC5'}
+                href={'https://coderdojo-cambridge.herokuapp.com/'}
+              >
+                coderdojo-cambridge.herokuapp.com
+              </Link>
+            </Heading>{' '}
+            <Box w={'200px'}>
+              <FontAwesomeIcon icon={faMousePointer} />{' '}
+              <Heading size={'xs'}>Go to this link to get here...</Heading>
+            </Box>
+          </HStack>
 
           <VStack spacing={2} ali w={'full'} h={'full'}>
             {data.map((proj) => (
